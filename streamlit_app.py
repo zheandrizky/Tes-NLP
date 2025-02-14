58875ff1a194c47a0b80d33b0619a964129fbdbb
Zheand Rizky Pranasyach <82630637+zheandrizky@users.noreply.github.com>
2025-02-14 19:20:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 import pandas as pd
-from preprocess import preprocess_text
+from preprocessing import preprocess_text
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Memuat model dan TF-IDF vectorizer
